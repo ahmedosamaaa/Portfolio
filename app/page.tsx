0201044c7052fa69bdb3053676f3a7667b1ebb2e
Ahmed <ahmedosama12313@gmail.com>
2024-11-27 07:25:29 +0200

    make More button to show more projects and fitch projects from api

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
+import { client } from "@/sanity/lib/client";
+import { PROJECTS_QUERY } from "@/sanity/lib/queries";
 
-export default function Home() {
+export default async function Home() {
+    const projects = await client.fetch(PROJECTS_QUERY);
+
     return (
         <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
             <div className="max-w-7xl w-full">
                 <Hero />
                 <Grid />
-                <RecentProjects />
+                <RecentProjects projects={projects} />
                 <Experience />
                 <Approach />
             </div>
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,91 +1,127 @@
 "use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
-
-import { projects } from "@/data";
-
+import { HiCursorClick } from "react-icons/hi";
 import dynamic from "next/dynamic";
+import { useState } from "react";
+import { Project } from "@/sanity.types";
+import MagicButton from "./ui/MagicButton";
 const PinContainer = dynamic(
     () => import("./ui/3d-pin").then((mod) => mod.PinContainer),
     { ssr: false }
 );
 
-const RecentProjects = () => {
+const RecentProjects = ({ projects }: { projects: Project[] }) => {
+    const [visibleCount, setVisibleCount] = useState(5);
+    // Handler for loading more projects
+    console.log("projectLength", projects.length);
+    const handleLoadMore = () => {
+        if (visibleCount < projects.length) {
+            setVisibleCount((prev) => prev + 4); // Load 4 more projects
+        }
+    };
+    const handleReset = () => {
+        if (visibleCount > projects.length) {
+            setVisibleCount(4); // Load 4 more projects
+        }
+    };
+    console.log("visibleCount", visibleCount);
     return (
         <section id="projects" className="py-20">
             <h1 className="heading">
                 A small selection of{" "}
                 <span className="text-purple">recent projects</span>
             </h1>
-            <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-                {projects.map((item) => (
-                    <div
-                        className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-                        key={item.id}
-                    >
-                        <PinContainer title={item.link} href={item.link}>
-                            <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                                <div
-                                    className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                                    style={{ backgroundColor: "#13162D" }}
-                                >
-                                    <img src="/bg.png" alt="bgimg" />
-                                </div>
-                                <img
-                                    src={item.img}
-                                    alt="cover"
-                                    className="z-10 absolute bottom-1 rotate-3 w-5/6 rounded-xl"
-                                />
-                            </div>
-
-                            <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                                {item.title}
-                            </h1>
-
-                            <p
-                                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                                style={{
-                                    color: "#BEC1DD",
-                                    margin: "1vh 0",
-                                }}
-                            >
-                                {item.des}
-                            </p>
-
-                            <div className="flex items-center justify-between mt-7 mb-3">
-                                <div className="flex items-center">
-                                    {item.iconLists.map((icon, index) => (
-                                        <div
-                                            key={index}
-                                            className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                                            style={{
-                                                transform: `translateX(-${
-                                                    5 * index + 2
-                                                }px)`,
-                                            }}
-                                        >
-                                            <img
-                                                src={icon}
-                                                alt="icon5"
-                                                className="aspect-[4/3]  w-4/5 object-contain"
-                                            />
-                                        </div>
-                                    ))}
-                                </div>
-
-                                <div className="flex justify-center items-center">
-                                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                                        Check Live Site
-                                    </p>
-                                    <FaLocationArrow
-                                        className="ms-3"
-                                        color="#CBACF9"
+            <div className="mt-28">
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center justify-center p-4 gap-12 md:gap-8 xl:gap-16 mt-10">
+                    {projects.slice(0, visibleCount).map((item) => (
+                        <div
+                            className="h-[20rem] lg:min-h-[32.5rem]   items-center justify-center "
+                            key={item._id}
+                        >
+                            <PinContainer title={item.link} href={item.link}>
+                                <div className="relative flex items-center justify-center w-[80vw] sm:w-96 md:w-[290px] lg:w-80 xl:w-[22rem] overflow-hidden h-[15vh] xl:h-[20vh] mb-10">
+                                    <div
+                                        className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                                        style={{ backgroundColor: "#13162D" }}
+                                    >
+                                        <img src="/bg.png" alt="bgimg" />
+                                    </div>
+                                    <img
+                                        src={item?.image?.url}
+                                        alt="cover"
+                                        className="z-10 absolute bottom-1 rotate-3 w-5/6 rounded-xl"
                                     />
                                 </div>
-                            </div>
-                        </PinContainer>
-                    </div>
-                ))}
+
+                                <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                                    {item.title}
+                                </h1>
+
+                                <p
+                                    className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
+                                    style={{
+                                        color: "#BEC1DD",
+                                        margin: "1vh 0",
+                                    }}
+                                >
+                                    {item.description}
+                                </p>
+
+                                <div className="flex items-center justify-between mt-7 mb-3">
+                                    <div className="flex items-center">
+                                        {item.projectStack &&
+                                            item?.projectStack.map(
+                                                (icon, index) => (
+                                                    <div
+                                                        key={index}
+                                                        className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                                                        style={{
+                                                            transform: `translateX(-${
+                                                                5 * index + 2
+                                                            }px)`,
+                                                        }}
+                                                    >
+                                                        <img
+                                                            src={icon?.url}
+                                                            alt="icon5"
+                                                            className="aspect-[4/3]  w-4/5 object-contain"
+                                                        />
+                                                    </div>
+                                                )
+                                            )}
+                                    </div>
+
+                                    <div className="flex justify-center items-center">
+                                        <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                                            Check Live Site
+                                        </p>
+                                        <FaLocationArrow
+                                            className="ms-3"
+                                            color="#CBACF9"
+                                        />
+                                    </div>
+                                </div>
+                            </PinContainer>
+                        </div>
+                    ))}
+                </div>
+                {projects.length == visibleCount ? null : projects.length >
+                  visibleCount ? (
+                    <MagicButton
+                        handleClick={handleLoadMore}
+                        title="Show More"
+                        icon={<HiCursorClick />}
+                        position="right"
+                    />
+                ) : (
+                    <MagicButton
+                        handleClick={handleReset}
+                        title="Show Less"
+                        icon={<HiCursorClick />}
+                        position="right"
+                    />
+                )}
             </div>
         </section>
     );
